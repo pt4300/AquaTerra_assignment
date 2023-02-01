398e2621a20909deee8cb90a7dc6b57615145a73
Yuting Yu <pt4300@hotmail.com>
2023-02-01 18:14:05 +1100

    complete re-fetch button and apply css

--- dog_demo/src/DogPage.js
+++ dog_demo/src/DogPage.js
@@ -1,44 +1,69 @@
 import { React, useEffect, useState } from 'react';
+import './DogPage.css';
 
 const DogPage = () => {
   const [pictureArray, setPictureArray] = useState([]);
-
-  const getPictureURL = async () => {
-    return fetch('https://random.dog/woof.json').then((response) =>
-      response
-        .json()
-        .then((json) => {
-          if (!response.ok) {
-            return Promise.reject(json);
-          }
-          return json;
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-    );
-  };
-  useEffect(() => {
-    if (pictureArray.length < 8) {
-      getPictureURL().then((data) => {
-        let valid = data.url.includes('.jpg') || data.url.includes('.png') || data.url.includes('.gif');
-        if (valid) {
-          setPictureArray((fetchedPicture) => [...fetchedPicture, data.url]);
-        }
-      });
+  const [index, setIndex] = useState(0);
+  async function fetchData() {
+    //re-fetch images if the array does not contain 8 images
+    if (index >= 8) {
+      console.log(pictureArray);
+      return;
     }
-  }, [pictureArray]);
+    // initial the request
+    const res = await fetch(`https://random.dog/woof.json`);
+    const data = await res.json();
+    //only accepting valid type picture
+    if (
+      data.url.toLowerCase().includes('jpg') ||
+      data.url.toLowerCase().includes('jpeg') ||
+      data.url.toLowerCase().includes('png') ||
+      data.url.toLowerCase().includes('mp4') ||
+      data.url.toLowerCase().includes('gif')
+    ) {
+      setIndex((prevIndex) => prevIndex + 1);
+      setPictureArray((fetchedPicture) => [...fetchedPicture, data.url]);
+    } else {
+      setIndex((prevIndex) => prevIndex);
+    }
+  }
+
+  const generateNewImages = () => {
+    setPictureArray([]);
+    setIndex(0);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [index, pictureArray]);
 
   return (
     <div>
-      hello
-      {pictureArray.map((picture) => (
-        <div>
-          {console.log(pictureArray)}
-          {picture}
-          <img src={picture} alt={'/'} />
-        </div>
-      ))}
+      <div className="title">Welcome to Dog collection</div>
+      <div className="gridContainer">
+        {pictureArray.map((image) => (
+          <div>
+            <div className="gridItem" key={image}>
+              {image.endsWith('.mp4') ? (
+                <video className="gridItem" src={image} controls />
+              ) : (
+                <img className="gridItem" src={image} alt="Image" />
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="buttonContainer">
+        <button
+          className="fetchButton"
+          type="button"
+          onClick={() => {
+            generateNewImages();
+          }}
+        >
+          Fetch New Images
+        </button>
+      </div>
     </div>
   );
 };
